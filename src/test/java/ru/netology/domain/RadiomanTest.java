a4682f40b38b009cbd0d81e3ec82fae3ee4156b0
Mikhail19870105 <crazyhtc87@gmail.com>
2020-05-28 14:17:51 +0300

    Added RadiomanTest.java

---
+++ src/test/java/ru/netology/domain/RadiomanTest.java
@@ -0,0 +1,46 @@
+package ru.netology.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadiomanTest {
+
+    @org.junit.jupiter.api.Test
+    void setMaxStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setMinStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setMaxVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setMinVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void upVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void downVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nextStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prevStation() {
+    }
+}
